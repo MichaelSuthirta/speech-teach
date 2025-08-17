f8fe14ca271a8b10c93e65a4e053c711cd49a27b
MichaelSuthirta <michaelsuthirta@gmail.com>
2025-08-17 15:06:41 +0700

    Feat: Created placeholder practice page and routing

--- Backend/Scripts/SpeechTeach_backend.py
+++ Backend/Scripts/SpeechTeach_backend.py
@@ -8,11 +8,14 @@ import numpy as np
 import pandas as pd
 
 # Random word generator
-dictionary = (pd.read_csv("Backend\\Resource\\english Dictionary.csv")).to_dict()
+def load_csv():
+    dictionary = (pd.read_csv("Backend\\Resource\\english Dictionary.csv")).to_dict()
+    return dictionary
 
-index = np.random.randint(low=0, high=len(dictionary["word"]))
-print(index)
+def pick_word(dictionary):
+    index = np.random.randint(low=0, high=len(dictionary["word"]))
+    print(index)
 
-chosen_word = dictionary["word"][index]
-word_meaning = dictionary["def"][index]
-print(chosen_word, ", definition: ", word_meaning)
+    chosen_word = dictionary["word"][index]
+    word_meaning = dictionary["def"][index]
+    print(chosen_word, ", definition: ", word_meaning)
