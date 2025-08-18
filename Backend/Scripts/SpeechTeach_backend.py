ef0b6f447dc5c056ee8fe6ee1d7f9b618a9819ac
MichaelSuthirta <michaelsuthirta@gmail.com>
2025-08-18 14:48:51 +0700

    Feat: Created fetcher for random word generator in backend, connected emulator to Flask.

--- Backend/Scripts/SpeechTeach_backend.py
+++ Backend/Scripts/SpeechTeach_backend.py
@@ -1,21 +1,43 @@
 # Flask
 from flask import Flask, jsonify
 
-# Numeric dependencies
+# Dependencies
 import numpy as np
-
-# Random word dependencies
 import pandas as pd
+
+app = Flask("__name__")
+
+dictionary = None
 
 # Random word generator
 def load_csv():
-    dictionary = (pd.read_csv("Backend\\Resource\\english Dictionary.csv")).to_dict()
-    return dictionary
+    global dictionary
+    if(dictionary == None):
+        dictionary = (pd.read_csv("Backend\\Resource\\english Dictionary.csv")).to_dict()
 
-def pick_word(dictionary):
+def pick_word():
+    if(dictionary == None):
+        load_csv()
+        
     index = np.random.randint(low=0, high=len(dictionary["word"]))
     print(index)
 
     chosen_word = dictionary["word"][index]
     word_meaning = dictionary["def"][index]
-    print(chosen_word, ", definition: ", word_meaning)
+    word_meaning = word_meaning.split(";")[0]
+
+    return chosen_word, word_meaning
+
+# Routes
+
+@app.route("/")
+def print_status():
+    return "Server on."
+
+@app.route("/word", methods=["GET"])
+def choose_word():
+    word, definition = pick_word()
+    return jsonify({'Word': word, 'Definition':definition})
+
+if __name__ == "__main__":
+    app.run(debug="True") 
